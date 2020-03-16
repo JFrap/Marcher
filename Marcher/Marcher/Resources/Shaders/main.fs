f3e2d72cfb368ec47b1a3bbe68c5bf4f19378147
James P <jameslind01@yahoo.co.uk>
2020-03-16 23:34:12 +0100

    imgui

--- Marcher/Marcher/Resources/Shaders/main.fs
+++ Marcher/Marcher/Resources/Shaders/main.fs
@@ -1,13 +1,10 @@
 #version 330 core
 out vec4 FragColor;
 
-#define MAX_MARCHING_STEPS 512
-#define MAX_DISTANCE 50.f
+#define MAX_MARCHING_STEPS 1024
+#define MAX_DISTANCE 200.f
 
-#define SHADOW_MAX_MARCHING_STEPS 128
-#define SHADOW_MAX_DISTANCE 50.f
-
-#define EPSILON 0.005f
+//#define EPSILON 0.005f
 
 struct Camera {
     vec3 Position, Target;
@@ -18,11 +15,16 @@ struct Ray {
     vec3 Origin, Direction;
 };
 
+uniform float EPSILON;
+uniform bool ShadowsEnabled;
+uniform float ShadowStrength;
+uniform vec3 AmbientColor;
+
 uniform vec2 ScreenSize;
 uniform Camera MainCamera;
 uniform float Time;
 
-uniform sampler3D Model;
+//uniform sampler3D Model;
 
 Ray CalculateFragRay() {
     vec2 RelScreenPos = gl_FragCoord.xy / ScreenSize;
@@ -33,8 +35,80 @@ Ray CalculateFragRay() {
     return Ray(FinalPos, normalize(FinalPos-MainCamera.Position));
 }
 
+vec4 permute(vec4 x) {return mod(((x*34.0)+1.0)*x, 289.0);}
+vec4 taylorInvSqrt(vec4 r) {return 1.79284291400159 - 0.85373472095314 * r;}
+vec3 fade(vec3 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
+
+float cnoise(vec3 P){
+    vec3 Pi0 = floor(P); // Integer part for indexing
+    vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
+    Pi0 = mod(Pi0, 289.0);
+    Pi1 = mod(Pi1, 289.0);
+    vec3 Pf0 = fract(P); // Fractional part for interpolation
+    vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+    vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+    vec4 iy = vec4(Pi0.yy, Pi1.yy);
+    vec4 iz0 = Pi0.zzzz;
+    vec4 iz1 = Pi1.zzzz;
+
+    vec4 ixy = permute(permute(ix) + iy);
+    vec4 ixy0 = permute(ixy + iz0);
+    vec4 ixy1 = permute(ixy + iz1);
+
+    vec4 gx0 = ixy0 / 7.0;
+    vec4 gy0 = fract(floor(gx0) / 7.0) - 0.5;
+    gx0 = fract(gx0);
+    vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+    vec4 sz0 = step(gz0, vec4(0.0));
+    gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+    gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+
+    vec4 gx1 = ixy1 / 7.0;
+    vec4 gy1 = fract(floor(gx1) / 7.0) - 0.5;
+    gx1 = fract(gx1);
+    vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+    vec4 sz1 = step(gz1, vec4(0.0));
+    gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+    gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+
+    vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+    vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+    vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+    vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+    vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+    vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+    vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+    vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+
+    vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+    g000 *= norm0.x;
+    g010 *= norm0.y;
+    g100 *= norm0.z;
+    g110 *= norm0.w;
+    vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+    g001 *= norm1.x;
+    g011 *= norm1.y;
+    g101 *= norm1.z;
+    g111 *= norm1.w;
+
+    float n000 = dot(g000, Pf0);
+    float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+    float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+    float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+    float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+    float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+    float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+    float n111 = dot(g111, Pf1);
+
+    vec3 fade_xyz = fade(Pf0);
+    vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+    vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+    float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); 
+    return 2.2 * n_xyz;
+}
+
 float sphereSDF(in vec3 rPos, in vec3 sPos) {
-    return distance(rPos, sPos) - 1.2;
+    return distance(rPos, sPos) - 1;
 }
 
 vec3 SDFRepitition(in vec3 p, in vec3 c) {
@@ -42,26 +116,54 @@ vec3 SDFRepitition(in vec3 p, in vec3 c) {
     return q;
 }
 
-
 float opSmoothUnion(float d1, float d2, float k) {
     float h = clamp( 0.5 + 0.5*(d2-d1)/k, 0.0, 1.0 );
     return mix( d2, d1, h ) - k*h*(1.0-h); 
 }
 
-float sdBox( vec3 p, vec3 bpos, vec3 b ) {
-  vec3 d = distance(abs(p), abs(bpos)) - b;
+float sdBox( vec3 p, vec3 b ) {
+  vec3 d = abs(p) - b;
   return length(max(d,0.0))
          + min(max(d.x,max(d.y,d.z)),0.0); // remove this line for an only partially signed sdf 
 }
 
 float SceneSDF(in vec3 p) {
-    return min(texture(Model, p * vec3(1.f/2)).r, p.y);
+    // float s = 100000;
+    // if (sdBox(p - vec3(1), vec3(1)) <= EPSILON) {
+    //     s = texture(Model, p * vec3(1.f/2)).r;
+    // }
+    // else {
+    //     s = sdBox(p - vec3(1), vec3(1));
+    // }
+    // float f = sphereSDF(SDFRepitition(p, vec3(6,0,6)), vec3(0));
+    // if (f <= EPSILON) {
+    //     //f = max(f, cnoise(p+vec3(Time)));
+    // }
 
-    //return min(sdBox(p, vec3(1)), sphereSDF(p, vec3(0)));
+    // return min(min(s, f), p.y);
+
+    return min(sphereSDF(SDFRepitition(p, vec3(6,6,6)), vec3(0)), p.y);
+}
+
+float SceneSDFAO(in vec3 p) {
+    // float s = 100000;
+    // if (sdBox(p - vec3(1), vec3(1)) <= EPSILON) {
+    //     s = texture(Model, p * vec3(1.f/2)).r;
+    // }
+    // else if (sdBox(p - vec3(1), vec3(1)) <= 0.5) {
+    //     s = sdBox(p - vec3(1), vec3(1)) + texture(Model, p * vec3(1.f/2)).r;
+    // }
+    // float f = sphereSDF(SDFRepitition(p, vec3(6,0,6)), vec3(0));
+    // if (f <= EPSILON) {
+    //     //f = max(f, cnoise(p+vec3(Time)));
+    // }
+
+    // return min(min(s, f), p.y);
+    return min(sphereSDF(SDFRepitition(p, vec3(6,6,6)), vec3(0)), p.y);
 }
 
 vec3 EstimateNormal(in vec3 p) {
-    const vec3 small_step = vec3(0.001, 0.0, 0.0);
+    vec3 small_step = vec3(EPSILON, 0.0, 0.0);
 
     float gradient_x = SceneSDF(p + small_step.xyy) - SceneSDF(p - small_step.xyy);
     float gradient_y = SceneSDF(p + small_step.yxy) - SceneSDF(p - small_step.yxy);
@@ -76,57 +178,83 @@ struct MarchInfo {
     bool Hit;
     float Depth, MinDistance;
     vec3 Position, Normal;
+    int Steps;
 };
 
 MarchInfo March(in Ray ray) {
     float depth = 0.f;
     float dist, minDist = MAX_DISTANCE;
-    for (int i = 0; i < MAX_MARCHING_STEPS; i++) {
+    int i = 0;
+    for (; i < MAX_MARCHING_STEPS; i++) {
         dist = SceneSDF(ray.Origin + (ray.Direction * depth));
         minDist = min(dist, minDist);
-        if (abs(dist) < EPSILON) {
-
-            return MarchInfo(true, depth, dist, ray.Origin + (ray.Direction * depth), EstimateNormal(ray.Origin + (ray.Direction * depth)));
+        if (dist < EPSILON) {
+            if (dist < 0) {
+                depth += dist; depth += dist;
+            }
+            return MarchInfo(true, depth, dist, ray.Origin + (ray.Direction * depth), EstimateNormal(ray.Origin + (ray.Direction * depth)), i);
         }
         depth += dist;
         if (depth >= MAX_DISTANCE) {
-            return MarchInfo(false, depth, minDist, ray.Origin + (ray.Direction * depth), vec3(0));
+            return MarchInfo(false, depth, minDist, ray.Origin + (ray.Direction * depth), vec3(0), i);
         }
     }
-    return MarchInfo(false, depth, minDist, ray.Origin + (ray.Direction * depth), vec3(0));
+    return MarchInfo(false, depth, minDist, ray.Origin + (ray.Direction * depth), vec3(0), i);
 }
 
 float SoftShadow(in Ray ray, in float k) {
-    float res = 1.0;
-    float ph = 1e20;
-    for(float t=EPSILON; t<SHADOW_MAX_DISTANCE;) {
+    for(float t=EPSILON; t<MAX_DISTANCE;) {
         float h = SceneSDF(ray.Origin + ray.Direction*t);
         if(h<EPSILON)
             return 0;
-        float y = h*h/(2.0*ph);
-        float d = sqrt(h*h-y*y);
-        res = min(res, k*d/max(0.0,t-y));
-        ph = h;
         t += h;
     }
-    return res;
+    return 1;
+}
+
+float genAmbientOcclusion(vec3 ro, vec3 rd) {
+    vec4 totao = vec4(0.0);
+    float sca = 1.0;
+
+    for (int aoi = 0; aoi < 5; aoi++) {
+        float hr = 0.01 + 0.02 * float(aoi * aoi);
+        vec3 aopos = ro + rd * hr;
+        float dd = SceneSDF(aopos);
+        float ao = clamp(-(dd - hr), 0.0, 1.0);
+        totao += ao * sca * vec4(1.0, 1.0, 1.0, 1.0);
+        sca *= 0.75;
+    }
+
+    const float aoCoef = 1;
+    return 1.0 - clamp(aoCoef * totao.w, 0.0, 1.0);
 }
 
 vec3 Render(in Ray ray) {
     MarchInfo info = March(ray);
 
     if (info.Hit) {
-        float shadow = 1.f-SoftShadow(Ray(info.Position + info.Normal * EPSILON, normalize(vec3(1))), 8);
+        float shadow = 0.f;
+        if (ShadowsEnabled) {
+            shadow = 1.f-SoftShadow(Ray(info.Position + info.Normal * EPSILON, normalize(vec3(1))), 8);
+        }
+        
+        vec3 ret = vec3(max(dot(info.Normal, normalize(vec3(1))), 0.f));
+        ret -= vec3(shadow * ShadowStrength) * ret;
+        ret += AmbientColor * (vec3(1)-ret);
 
-        float ret = max(dot(info.Normal, normalize(vec3(1))), 0.1f);
-        ret -= shadow * (ret-0.1f);
-        return mix(ret * vec3(1.f), vec3(0.5f), distance(ray.Origin, info.Position)/MAX_DISTANCE);
+        ret *= genAmbientOcclusion(info.Position + info.Normal * EPSILON, info.Normal);
+        return mix(ret * vec3(1.f), AmbientColor, distance(ray.Origin, info.Position)/MAX_DISTANCE);
     }
-    return vec3(0.5f);
+    return AmbientColor;
 }
 
 void main() {
     Ray CamRay = CalculateFragRay();
 
-    FragColor = vec4(Render(CamRay), 1.f);
+    if (SceneSDF(CamRay.Origin) < EPSILON) {
+        FragColor = vec4(0,0,0,1);
+    }
+    else {
+        FragColor = vec4(Render(CamRay), 1.f);
+    }
 }
--- Marcher/Marcher/main.cpp
+++ Marcher/Marcher/main.cpp
@@ -6,6 +6,98 @@
 #include "Engine/Timer.h"
 #include "Engine/Graphics/VolumetricModel.h"
 
+
+#include "Engine/ImGUI/imgui.h"
+#include "Engine/ImGUI/imgui-SFML.h"
+
+// F E A R L E S S C O N C U R R E N C Y
+float MouseSensitivity = 1.f;
+float MovementSpeed = 5.f;
+float FieldOfView = 90.f;
+
+float Epsilon = 0.005f;
+bool ShadowsEnabled = true;
+float ShadowStrength = 1.f;
+glm::vec3 AmbientColor = glm::vec3(0.05f);
+
+bool AAEnabled = true;
+
+bool running = true;
+bool VSync = true;
+float MainFPS = 0.f;
+float MainMS = 0.f;
+
+void UtiltiyWindow(sf::Window *mainWindow) {
+	sf::RenderWindow window(sf::VideoMode(200, 720), "Settings");
+	window.setPosition(mainWindow->getPosition() + sf::Vector2i(mainWindow->getSize().x, 0));
+	window.setVerticalSyncEnabled(true);
+
+	ImGui::SFML::Init(window);
+
+	bool attached = true;
+
+	sf::Clock clock;
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			ImGui::SFML::ProcessEvent(event);
+			if (!running) {
+				window.close();
+			}
+		}
+		if (attached && mainWindow) {
+			window.setPosition(mainWindow->getPosition() + sf::Vector2i(mainWindow->getSize().x, 0));
+		}
+
+		sf::Time delta = clock.restart();
+		ImGui::SFML::Update(window, delta);
+
+		ImGui::Begin("Settings", nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar);
+		ImGui::SetWindowPos(ImVec2(0, 0));
+		ImGui::SetWindowSize(ImVec2(window.getSize().x, window.getSize().y));
+
+		ImGui::Text("Performance");
+		ImGui::Text(("FPS: " + std::to_string(MainFPS)).c_str());
+		ImGui::Text(("MS: " + std::to_string(MainMS)).c_str());
+		ImGui::Checkbox("VSync", &VSync);
+
+		if (ImGui::CollapsingHeader("Meta")) {ImGui::PushItemWidth(-100);
+			ImGui::Checkbox("Attach To Main Window", &attached);
+		}
+		ImGui::PushItemWidth(-100);
+		if (ImGui::CollapsingHeader("Camera")) {
+			ImGui::SliderFloat("Sensitivity", &MouseSensitivity, 0.1f, 5.f, "%.1f");
+			ImGui::Spacing();
+			ImGui::SliderFloat("Speed", &MovementSpeed, 1.f, 20.f, "%.1f");
+			ImGui::Spacing();
+			ImGui::SliderFloat("FOV", &FieldOfView, 1.f, 120.f, "%.1f");
+		}
+		ImGui::PushItemWidth(-75);
+		if (ImGui::CollapsingHeader("Rendering")) {
+			ImGui::InputFloat("Epsilon", &Epsilon, 0.0005f, -0.0005f, "%.5f");
+			if (ImGui::CollapsingHeader("Shadows")) {
+				ImGui::Checkbox("Enable Shadows", &ShadowsEnabled);
+				ImGui::Spacing();
+				ImGui::PushItemWidth(-110);
+				ImGui::SliderFloat("Shadow Strength", &ShadowStrength, 0.f, 1.f, "%.2f");
+				ImGui::Spacing();
+				ImGui::ColorPicker3("Ambient Light", &AmbientColor[0]);
+			}
+		}
+
+		ImGui::PopItemWidth();
+		ImGui::End();
+
+
+
+		window.clear(sf::Color(128, 128, 128));
+		ImGui::SFML::Render(window);
+		window.display();
+	}
+
+	ImGui::SFML::Shutdown();
+}
+
 int main() {
 	sf::ContextSettings settings;
 	settings.depthBits = 24;
@@ -14,7 +106,10 @@ int main() {
 	settings.minorVersion = 3;
 
 	sf::Window window(sf::VideoMode(1280, 720), "Marcher", sf::Style::Default);
-	window.setVerticalSyncEnabled(true);
+	//window.setFramerateLimit(120);
+
+	sf::Thread UtilityThread(UtiltiyWindow, &window);
+	UtilityThread.launch();
 
 	int gladInitRes = gladLoadGL();
 	if (!gladInitRes) {
@@ -52,7 +147,7 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
-	marcher::VolumetricModel model("bunny.vol");
+	//marcher::VolumetricModel model("bunny.vol");
 
 	glm::vec3 cameraRot = glm::vec3();
 	marcher::Camera camera = marcher::Camera(glm::vec3(0, 0, 1), glm::vec3(0, 0, 0));
@@ -63,13 +158,17 @@ int main() {
 
 	bool active = true;
 	while (window.isOpen()) {
+		window.setVerticalSyncEnabled(VSync);
 		float dt = timer.Restart<float>();
 		totalTime += dt;
 		
+		
 		sf::Event event;
 		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed)
+			if (event.type == sf::Event::Closed) {
 				window.close();
+				running = false;
+			}
 			else if (event.type == sf::Event::Resized) {
 				glViewport(0, 0, window.getSize().x, window.getSize().y);
 			}
@@ -80,36 +179,40 @@ int main() {
 				}
 			}
 			else if (event.type == sf::Event::LostFocus) {
+				window.setMouseCursorVisible(true);
 				active = false;
 			}
 			else if (event.type == sf::Event::GainedFocus) {
 				active = true;
+				window.setMouseCursorVisible(false);
 			}
 		}
 
 		TotalFrames++;
 		float currentTime = frameTimer.CurrentTime<float>();
-		if (currentTime >= 1.f) {
+		if (currentTime >= 0.5f) {
 			printf("%f FPS | %f MS \n", (float)TotalFrames / currentTime, (currentTime / (float)TotalFrames) * 1000.f);
+			MainFPS = (float)TotalFrames / currentTime;
+			MainMS = (currentTime / (float)TotalFrames) * 1000.f;
 			frameTimer.Restart();
 			TotalFrames = 0;
 		}
 
 		sf::Vector2i windowCenter = sf::Vector2i(window.getSize().x / 2, window.getSize().y / 2);
 		if (sf::Mouse::getPosition(window) != windowCenter && active) {
-			cameraRot.y += ((float)sf::Mouse::getPosition(window).x - (float)windowCenter.x) / 1000;
-			cameraRot.x += ((float)sf::Mouse::getPosition(window).y - (float)windowCenter.y) / 1000;
+			cameraRot.y += (((float)sf::Mouse::getPosition(window).x - (float)windowCenter.x) / 1000) * MouseSensitivity;
+			cameraRot.x += (((float)sf::Mouse::getPosition(window).y - (float)windowCenter.y) / 1000) * MouseSensitivity;
 			sf::Mouse::setPosition(windowCenter, window);
 		}
 
 		auto camDir = glm::vec3(glm::vec4(0, 0, -1, 1) * glm::rotate(cameraRot.x, glm::vec3(1, 0, 0)) * glm::rotate(cameraRot.y, glm::vec3(0, 1, 0)));
 		auto camRight = glm::vec3(glm::vec4(1, 0, 0, 1) * glm::rotate(cameraRot.y, glm::vec3(0, 1, 0)));
 
-		float speed = 5.f;
+		float speed = MovementSpeed;
 
 		if (active) {
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
-				speed = 20;
+				speed *= 2;
 			}
 
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
@@ -142,14 +245,23 @@ int main() {
 		glClearColor(0.5f, 0.5f, 0.5f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		camera.FOV = FieldOfView;
 		camera.Update(mainShader, (float)window.getSize().x / (float)window.getSize().y, glm::vec4(0, 0, window.getSize().x, window.getSize().y));
 		mainShader->SendUniform("ScreenSize", glm::vec2(window.getSize().x, window.getSize().y));
 		mainShader->SendUniform("Time", totalTime);
 
+		if (Epsilon <= 0) {
+			Epsilon = 0.0005f;
+		}
+		mainShader->SendUniform("EPSILON", Epsilon);
+		mainShader->SendUniform("ShadowsEnabled", (int)ShadowsEnabled);
+		mainShader->SendUniform("ShadowStrength", ShadowStrength);
+		mainShader->SendUniform("AmbientColor", AmbientColor);
+
 		mainShader->Bind();
 
-		model.Bind(0);
-		mainShader->SendUniform("Model", 0);
+		/*model.Bind(0);
+		mainShader->SendUniform("Model", 0);*/
 
 		glBindVertexArray(VAO); 
 		glDrawArrays(GL_TRIANGLES, 0, 6);
@@ -161,6 +273,8 @@ int main() {
 	
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+
+	UtilityThread.wait();
 
 	return 0;
 }
